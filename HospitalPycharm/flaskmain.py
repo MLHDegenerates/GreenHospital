9ec923e2f2c813249fe748388856d2eab54b72a6
xdrgb22 <17xjem@queensu.ca>
2018-02-03 07:25:17 -0500

    add user finish again

--- HospitalPycharm/flaskmain.py
+++ HospitalPycharm/flaskmain.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 users = []
+doctors = []
+nurses = []
 
 @app.route("/")
 def hello():
@@ -26,6 +28,43 @@ def addpatient():
 @app.route("/accesspatients")
 def accesspatients():
     return render_template("accesspatients.html", users=users)
+
+@app.route("/addingdoctor")
+def addingdoctor():
+    return render_template("addingdoctor.html")
+
+
+@app.route("/adddoctor")
+def adddoctor():
+    doctors.append({"first": request.args.get("fname"), "second": request.args.get("lname")})
+    print("added")
+    print(request.args.get("fname"))
+    print(request.args.get("lname"))
+    return redirect("/")
+
+
+@app.route("/accessdoctors")
+def accessdoctors():
+    return render_template("accessdoctors.html", doctors=doctors)
+
+@app.route("/addingnurse")
+def addingnurse():
+    return render_template("addingnurse.html")
+
+
+@app.route("/addnurse")
+def addnurse():
+    nurses.append({"first": request.args.get("fname"), "second": request.args.get("lname")})
+    print("added")
+    print(request.args.get("fname"))
+    print(request.args.get("lname"))
+    return redirect("/")
+
+@app.route("/accessnurses")
+def accessnurses():
+    return render_template("accessnurses.html", nurses=nurses)
+
+
 
 
 
